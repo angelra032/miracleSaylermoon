f3a267fd44496030e06c2ff037a65162de96f23b
minjin8932 <minjin032@naver.com>
2021-05-11 17:23:16 +0900

    minjin pm0523 update reservation

--- src/main/java/com/donzzul/spring/reservation/service/logic/ReservationServiceImpl.java
+++ src/main/java/com/donzzul/spring/reservation/service/logic/ReservationServiceImpl.java
@@ -1,14 +1,72 @@
 package com.donzzul.spring.reservation.service.logic;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.donzzul.spring.reservation.store.ReservationStrore;
+import com.donzzul.spring.reservation.domain.Reservation;
+import com.donzzul.spring.reservation.service.ReservationService;
+import com.donzzul.spring.reservation.store.ReservationStore;
 
 @Service
-public class ReservationServiceImpl {
+public class ReservationServiceImpl implements ReservationService{
 
 	
 	@Autowired
-	private ReservationStrore store;
+	private ReservationStore store;
+
+	@Override
+	public int insertReservation(Reservation reservation) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public HashMap<String, String> reservaionListByDream(int reservationNo, int userNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<String, String> reservationListByMZ(int reservationNo, int userNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<String, String> reservaionListByShop(int reservationNo, int shopNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteReservation(int reservationNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateReservation(Reservation reservation) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int comfirmReservation(int reservationNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int cancleReservation(int reservationNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int completeReservation(int reservationNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
--- src/main/java/com/donzzul/spring/reservation/store/logic/ReservationStoreLogic.java
+++ src/main/java/com/donzzul/spring/reservation/store/logic/ReservationStoreLogic.java
@@ -1,12 +1,70 @@
 package com.donzzul.spring.reservation.store.logic;
 
 import java.sql.SQLSyntaxErrorException;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.donzzul.spring.reservation.domain.Reservation;
+import com.donzzul.spring.reservation.store.ReservationStore;
+
 @Repository
-public class ReservationStoreLogic {
+public class ReservationStoreLogic implements ReservationStore{
+
+	@Override
+	public int insertReservation(Reservation reserveReservation) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public HashMap<String, String> reservationListByDream(int reservationNo, int userNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<String, String> reservationListByMZ(int reservationNo, int userNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<String, String> reservationListByShop(int reservationNo, int ShopNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteReservation(int reservationNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateReservation(int reservationNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int comfirmReservation(int reservationNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int cancleReservation(int reservationNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int complteReservation(int reservationNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 //	@Autowired
 //	private SqlSessionTemplate sqlSession;
