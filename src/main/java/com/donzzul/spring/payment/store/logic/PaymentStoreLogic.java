761b049991949cf470f2afc607d64168bfd1727a
johyo <johyo@DESKTOP-0UKV3GQ>
2021-05-11 09:52:06 +0900

    0511 hyoju payment annotation

--- src/main/java/com/donzzul/spring/payment/store/logic/PaymentStoreLogic.java
+++ src/main/java/com/donzzul/spring/payment/store/logic/PaymentStoreLogic.java
@@ -1,10 +1,18 @@
 package com.donzzul.spring.payment.store.logic;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.donzzul.spring.payment.store.PaymentStore;
 import com.donzzul.spring.user.domain.User;
 
+@Repository
 public class PaymentStoreLogic implements PaymentStore{
 
+//	@Autowired
+//	private SqlSessionTemplate session;
+	
 	@Override
 	public int updatePoint(User user) {
 		// TODO Auto-generated method stub
