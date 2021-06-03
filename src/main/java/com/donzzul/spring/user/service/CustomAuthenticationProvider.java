a255bf0601981ef294b0288df2664f52089fda07
parkminae <parkminae111@naver.com>
2021-06-03 09:33:32 +0900

    0603_ma:CustomAuthenticationProvider

--- src/main/java/com/donzzul/spring/user/service/CustomAuthenticationProvider.java
+++ src/main/java/com/donzzul/spring/user/service/CustomAuthenticationProvider.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
 import com.donzzul.spring.user.domain.CustomUserDetails;
-
+ 
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 	@Autowired
 	private UserDetailsServiceImpl uServiceImpl;
